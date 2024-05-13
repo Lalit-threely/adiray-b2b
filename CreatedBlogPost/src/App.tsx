b19c1c1afbbe3204f484fd71c15c718fb13747e1
Rahul Kumar <rahulkhg980@gmail.com>
2024-05-13 17:32:11 +0530

    Made a user panel

--- CreatedBlogPost/src/App.tsx
+++ CreatedBlogPost/src/App.tsx
@@ -1,4 +1,9 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import CreateBlog from "./component/CreateBlogPost"
+import UpdateBlog from "./component/UpdateBlogPost"
+import Blog from "./component/Blog"
+import Blogpost from "./component/BlogPost"
+import Navbar from "./component/Navbar"
 
 
 function App() {
@@ -6,7 +11,17 @@ function App() {
 
   return (
     <>
-      <CreateBlog/>
+
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+      
+      <Route path="/admin/blog/" element={<Blog/>} />
+      <Route path="/admin/posts/" element={<CreateBlog/>} />
+      <Route path="/admin/posts/:postId" element={<UpdateBlog />} />
+      <Route path="/admin/blogposts/:postId" element={<Blogpost />} />
+    </Routes>
+    </BrowserRouter>
     </>
   )
 }
